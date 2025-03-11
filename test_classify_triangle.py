331719ccd43540283033afe964d3f46d070ba62b
RY-Xin <xinhuiyu0216@gmail.com>
2025-03-11 17:11:11 -0400

    Modify test cases for classifyTriangle function

--- test_classify_triangle.py
+++ test_classify_triangle.py
@@ -21,6 +21,20 @@ class TestClassifyTriangle(unittest.TestCase):
     def test_invalid_triangle(self):
         self.assertEqual(classify_triangle(1, 2, 3), "Invalid Triangle")
         self.assertEqual(classify_triangle(5, 1, 1), "Invalid Triangle")
+        
+    #added more testcases
+    def test_negative_and_zero_sides(self):
+        self.assertEqual(classify_triangle(-1, 2, 3), "Invalid Triangle")
+        self.assertEqual(classify_triangle(0, 2, 3), "Invalid Triangle")
+        
+    
+def test_invalid_triangle_sides(self):
+    self.assertEqual(classify_triangle(1, 2, 3), "Invalid Triangle")
+    self.assertEqual(classify_triangle(1, 10, 12), "Invalid Triangle")
+
+def test_large_values(self):
+    self.assertEqual(classify_triangle(1000000, 1000000, 1000000), "Equilateral Triangle")
+    
 
 if __name__ == "__main__":
     unittest.main()
